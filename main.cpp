7ef5fef838dbb089d0ea9d15806e72f29a3cf8fa
MUHAMMAD ILHAM AKBAR <112915107+Muhammadilhamakb@users.noreply.github.com>
2022-10-13 19:55:44 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,49 +4,18 @@ using namespace std;
 
 int main()
 {
-   cout << "muhammad ilham akbar, A11.2022.14109" << endl;
-   cout << "  DAFTAR JURUSAN"<< endl;
-   cout << "1.TEKNIK INFORMATIKA" << endl;
-   cout << "2.SISTEM INFORMASI" << endl;
-   cout << "3.DESAIN KOMUNIKASI VISUAL" << endl;
-   cout << "  KATEGORI MAHASISWA"<< endl;
-   cout << "1.SEMESTER 1-2 = FRESHMAN" << endl;
-   cout << "2.SEMESTER 3-4 = SOPHOMORE" << endl;
-   cout << "3.SEMESTER 5-6 = JUNIOR" << endl;
-   cout << "4.SEMESTER 7-8 = SENIOR" << endl;
+    cout << "Hello Wordl!" << endl;
+    cout << "my name is MUHAMMAD ILHAM AKBAR" << endl;
 
-
-
-
-   int a,b;
-   cout << "masukkan jurusan anda (No 1-3)= ";
-   cin >> a;
-   if (a == 1) {
-        cout << "jurusan anda TEKNIK INFORMATIKA" << endl;
-   }
-   if (a == 2) {
-        cout << "jurusan anda SISTEM INFORMASI" << endl;
-   }
-   if (a == 3) {
-        cout << "jurusan anda DESAIN KOMUNIKASI VISUAL" << endl;
-   }
-   cout << "masukkan semester anda (No 1-8)=" ;
-   cin >> b ;
-   if (b <= 2) {
-        cout << "anda adalah FRESHMAN" << endl;
-   }
-   if (b > 2 && b < 5) {
-        cout << "anda adalah SOPHOMORE " << endl;
-   }
-   if (b == 5 && b == 6) {
-        cout << "anda adalah JUNIOR" << endl;
-   }
-   if (b > 6 && b <= 8) {
-        cout << "anda adalah SENIOR" << endl;
-   }
-   if (b >= 9) {
-        cout << "anda adalah CALON DOSEN SEJATI" << endl;
-   }
+    int u = 17;
+    float t = 164;
+    char b = 'L';
+    string k = "demak";
+    cout << endl;
+    cout << "Umur:" << u << endl;
+    cout << "Tinggi badan:" << t << endl;
+    cout << "Ukuran baju:" << b << endl;
+    cout << "Kota asal:" << k << endl;
 
     return 0;
 }
